2cd59a0b1754a38061d2e588367847420f7e92ff
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-12 23:35:13 +0100

    Brew formula update for goreleaserpoc version 0.2.1

--- Formula/goreleaserpoc.rb
+++ Formula/goreleaserpoc.rb
@@ -1,9 +1,9 @@
 class Goreleaserpoc < Formula
   desc "A simple Go app to test out GoReleaser (POC)"
-  homepage "homebrew-goreleaserpoc"
-  url "https://github.com/Ilyes512/goreleaserpoc/releases/download/0.2.0/goreleaserpoc_0.2.0_macOS_64-bit.tar.gz"
-  version "0.2.0"
-  sha256 "d4b818e728efbd80420b807537ba0642506f5f635200696d28a0224b4bdb847b"
+  homepage "https://github.com/Ilyes512/goreleaserpoc"
+  url "https://github.com/Ilyes512/goreleaserpoc/releases/download/0.2.1/goreleaserpoc_0.2.1_macOS_64-bit.tar.gz"
+  version "0.2.1"
+  sha256 "c2fee9f922ee10c416eead964b3db9dbc58ec3ab249cccdf63b5cf79790bed79"
 
   def install
     bin.install "goreleaserpoc"
